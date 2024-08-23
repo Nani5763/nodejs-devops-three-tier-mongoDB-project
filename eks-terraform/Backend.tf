12945a9e9bbfb95ef5724ee98ee6496ab8b0f516
Pavan sai <pavanaws57@gmail.com>
2024-08-23 19:58:57 +0530

    updates on backend.tf

--- eks-terraform/Backend.tf
+++ eks-terraform/Backend.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket = "state-remote-store"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
     required_version = ">= 1.6.3"
